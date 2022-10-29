be9cbdc9423bd32d46dd01a88e490dc16526ca12
shenfive@gmail.com <Danny Shen>
2022-10-29 11:03:15 +0800

    整合 Firebase

--- A209GoogleSI/ViewController.swift
+++ A209GoogleSI/ViewController.swift
@@ -16,11 +16,45 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func gSingAction(_ sender: Any) {
-        print("hello")
+        guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+        
+        // Create Google Sign In configuration object.
+        let config = GIDConfiguration(clientID: clientID)
+        
+        // Start the sign in flow!
+        GIDSignIn.sharedInstance.signIn(with: config, presenting: self) { [unowned self] user, error in
+            
+            if let error = error {
+                // ...
+                print(error.localizedDescription)
+                return
+            }
+            
+            guard
+                let authentication = user?.authentication,
+                let idToken = authentication.idToken
+            else {
+                return
+            }
+            
+            let credential = GoogleAuthProvider.credential(withIDToken: idToken,
+                                                           accessToken: authentication.accessToken)
+            
+            Auth.auth().signIn(with: credential) { authResult, error in
+                if let error = error {
+                    print(error.localizedDescription)
+                    return
+                }
+                
+            }
+            
+        }
+        
     }
-    
-    
 }
+
+
+
 
